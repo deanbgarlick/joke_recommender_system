e1288e96f5500fb3899c80fa330431ce8e6dbcf4
Dean Garlick <deanbgarlick@gmail.com>
2020-05-18 13:17:31 +0100

    Add to data prep script so that it can be run from the command line

--- als_data_prep.py
+++ als_data_prep.py
@@ -1,21 +1,34 @@
+from pyspark.sql import SparkSession
 from pyspark.sql.functions import lit, countDistinct
 from pyspark.sql.types import StructField, StructType, IntegerType, DoubleType
 
-inputDF = spark.read.csv('s3://aws-emr-resources-257018485161-us-east-1/ratings_3.csv', header='true', inferSchema='true').withColumnRenamed('_c0', 'userID').drop('0')
 
-fields = [StructField('userID', IntegerType(), True), StructField('jokeID', IntegerType(), True), StructField('rating', DoubleType(), True)]
-schema = StructType(fields)
-allJokeRatingsDF = sqlContext.createDataFrame(sc.emptyRDD(), schema)
+if __name__ == '__main__':
 
-for jokeID in inputDF.columns:
-    if jokeID != 'userID':
-        if jokeID in ['1', '2']:
-            jokeRatings = inputDF.select(['userID', jokeID])
-            distinctJokeRatings = jokeRatings.select(countDistinct(jokeID)).collect()[0][0]
-            if distinctJokeRatings != 1:
-                jokeRatings = jokeRatings.withColumn('jokeID', lit(int(jokeID)))
-                jokeRatings = jokeRatings.withColumn("rating", jokeRatings[jokeID].cast(DoubleType()))
-                jokeRatings = jokeRatings.drop(jokeID)
-                allJokeRatingsDF = allJokeRatingsDF.union(jokeRatings)
+    spark = SparkSession \
+        .builder \
+        .appName("ALS_Data_Prep") \
+        .getOrCreate()
 
-allJokeRatingsDF.write.parquet("s3://aws-emr-resources-257018485161-us-east-1/ratings_3_als.parquet")
+    sc = spark.sparkContext
+
+    inputDF = spark.read.csv('s3://aws-emr-resources-257018485161-us-east-1/ratings_3.csv', header='true', inferSchema='true').withColumnRenamed('_c0', 'userID').drop('0')
+
+    fields = [StructField('userID', IntegerType(), True), StructField('jokeID', IntegerType(), True), StructField('rating', DoubleType(), True)]
+    schema = StructType(fields)
+    allJokeRatingsDF = sqlContext.createDataFrame(sc.emptyRDD(), schema)
+
+    for jokeID in inputDF.columns:
+        if jokeID != 'userID':
+            if jokeID in ['1', '2']:
+                jokeRatings = inputDF.select(['userID', jokeID])
+                distinctJokeRatings = jokeRatings.select(countDistinct(jokeID)).collect()[0][0]
+                if distinctJokeRatings != 1:
+                    jokeRatings = jokeRatings.withColumn('jokeID', lit(int(jokeID)))
+                    jokeRatings = jokeRatings.withColumn("rating", jokeRatings[jokeID].cast(DoubleType()))
+                    jokeRatings = jokeRatings.drop(jokeID)
+                    allJokeRatingsDF = allJokeRatingsDF.union(jokeRatings)
+
+    allJokeRatingsDF.write.parquet("s3://aws-emr-resources-257018485161-us-east-1/ratings_3_als.parquet")
+
+    spark.stop()
