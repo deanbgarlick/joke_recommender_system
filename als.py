f1a8195c71dce3d44a551d627aa7ee49ebc578a9
Dean Garlick <deanbgarlick@gmail.com>
2020-05-18 13:56:02 +0100

    Add code to fit and evaluate ALS model

--- als.py
+++ als.py
@@ -1,3 +1,5 @@
+from pyspark.ml.evaluation import RegressionEvaluator
+from pyspark.ml.recommendation import ALS
 from pyspark.sql import SparkSession
 from pyspark.sql.context import SQLContext
 
@@ -13,5 +15,18 @@ if __name__ == '__main__':
     sqlContext = SQLContext(sc)
 
     ratingsDF = spark.read.load("s3://aws-emr-resources-257018485161-us-east-1/ratings_3_als.parquet")
+
+    (training, test) = ratingsDF.randomSplit([0.8, 0.2])
+
+    als = ALS(maxIter=5, regParam=0.01, userCol="userId", itemCol="movieId", ratingCol="rating",
+              coldStartStrategy="drop")
+
+    model = als.fit(training)
+
+    predictions = model.transform(test)
+    evaluator = RegressionEvaluator(metricName="rmse", labelCol="rating",
+                                    predictionCol="prediction")
+    rmse = evaluator.evaluate(predictions)
+    print("Root-mean-square error = " + str(rmse))
 
     spark.stop()
