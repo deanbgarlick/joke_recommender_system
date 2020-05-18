ae2a66f90272ca1069ac41ab9caf85169184df80
Dean Garlick <deanbgarlick@gmail.com>
2020-05-18 14:01:08 +0100

    Lint files

--- als.py
+++ als.py
@@ -4,28 +4,34 @@ from pyspark.sql import SparkSession
 from pyspark.sql.context import SQLContext
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
-    spark = SparkSession \
-        .builder \
-        .appName("ALS_Data_Prep") \
-        .getOrCreate()
+    spark = SparkSession.builder.appName("ALS_Data_Prep").getOrCreate()
 
     sc = spark.sparkContext
     sqlContext = SQLContext(sc)
 
-    ratingsDF = spark.read.load("s3://aws-emr-resources-257018485161-us-east-1/ratings_3_als.parquet")
+    ratingsDF = spark.read.load(
+        "s3://aws-emr-resources-257018485161-us-east-1/ratings_3_als.parquet"
+    )
 
     (training, test) = ratingsDF.randomSplit([0.8, 0.2])
 
-    als = ALS(maxIter=5, regParam=0.01, userCol="userID", itemCol="jokeID", ratingCol="rating",
-              coldStartStrategy="drop")
+    als = ALS(
+        maxIter=5,
+        regParam=0.01,
+        userCol="userID",
+        itemCol="jokeID",
+        ratingCol="rating",
+        coldStartStrategy="drop",
+    )
 
     model = als.fit(training)
 
     predictions = model.transform(test)
-    evaluator = RegressionEvaluator(metricName="rmse", labelCol="rating",
-                                    predictionCol="prediction")
+    evaluator = RegressionEvaluator(
+        metricName="rmse", labelCol="rating", predictionCol="prediction"
+    )
     rmse = evaluator.evaluate(predictions)
     print("Root-mean-square error = " + str(rmse))
 
--- als_data_prep.py
+++ als_data_prep.py
@@ -4,32 +4,47 @@ from pyspark.sql.functions import lit, countDistinct
 from pyspark.sql.types import StructField, StructType, IntegerType, DoubleType
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
-    spark = SparkSession \
-        .builder \
-        .appName("ALS_Data_Prep") \
-        .getOrCreate()
+    spark = SparkSession.builder.appName("ALS_Data_Prep").getOrCreate()
 
     sc = spark.sparkContext
     sqlContext = SQLContext(sc)
 
-    inputDF = spark.read.csv('s3://aws-emr-resources-257018485161-us-east-1/ratings_3.csv', header='true', inferSchema='true').withColumnRenamed('_c0', 'userID').drop('0')
+    inputDF = (
+        spark.read.csv(
+            "s3://aws-emr-resources-257018485161-us-east-1/ratings_3.csv",
+            header="true",
+            inferSchema="true",
+        )
+        .withColumnRenamed("_c0", "userID")
+        .drop("0")
+    )
 
-    fields = [StructField('userID', IntegerType(), True), StructField('jokeID', IntegerType(), True), StructField('rating', DoubleType(), True)]
+    fields = [
+        StructField("userID", IntegerType(), True),
+        StructField("jokeID", IntegerType(), True),
+        StructField("rating", DoubleType(), True),
+    ]
     schema = StructType(fields)
     allJokeRatingsDF = sqlContext.createDataFrame(sc.emptyRDD(), schema)
 
     for jokeID in inputDF.columns:
-        if jokeID != 'userID':
-            if jokeID!='0':
-                jokeRatings = inputDF.select(['userID', jokeID])
-                jokeRatings = jokeRatings.filter((jokeRatings[jokeID]>10)&(jokeRatings[jokeID]>-10))
-                jokeRatings = jokeRatings.withColumn('jokeID', lit(int(jokeID)))
-                jokeRatings = jokeRatings.withColumn("rating", jokeRatings[jokeID].cast(DoubleType()))
+        if jokeID != "userID":
+            if jokeID != "0":
+                jokeRatings = inputDF.select(["userID", jokeID])
+                jokeRatings = jokeRatings.filter(
+                    (jokeRatings[jokeID] > 10) & (jokeRatings[jokeID] > -10)
+                )
+                jokeRatings = jokeRatings.withColumn("jokeID", lit(int(jokeID)))
+                jokeRatings = jokeRatings.withColumn(
+                    "rating", jokeRatings[jokeID].cast(DoubleType())
+                )
                 jokeRatings = jokeRatings.drop(jokeID)
                 allJokeRatingsDF = allJokeRatingsDF.union(jokeRatings)
 
-    allJokeRatingsDF.write.mode("overwrite").parquet("s3://aws-emr-resources-257018485161-us-east-1/ratings_3_als.parquet")
+    allJokeRatingsDF.write.mode("overwrite").parquet(
+        "s3://aws-emr-resources-257018485161-us-east-1/ratings_3_als.parquet"
+    )
 
     spark.stop()
