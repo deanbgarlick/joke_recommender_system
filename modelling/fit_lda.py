bb3a4f7d0193d1e71f5d2edec580fb68490ccb8d
Dean Garlick <deanbgarlick@gmail.com>
2020-05-20 23:48:56 +0100

    Add empty functions to recommend.py and move print topic fucntions to recommend.py

--- modelling/fit_lda.py
+++ modelling/fit_lda.py
@@ -5,22 +5,6 @@ from pyspark.sql import SparkSession
 from pyspark.sql.types import StructField, StructType, IntegerType, StringType, ArrayType
 
 
-def print_topic(topic, i):
-    print("\n\n")
-    print("Topic: " + str(i))
-    list(map(lambda x: print(str(x[0]) + ": " + str(x[1])), topic)) # x = ( term, weight)
-
-
-def print_topics(ldaPipelineModel):
-    countVectorizer = ldaPipelineModel.stages[3]
-    vocabList = countVectorizer.vocabulary
-    ldaModel = ldaPipelineModel.stages[4]
-    topicIndices = ldaModel.describeTopics(maxTermsPerTopic=5)
-    topics = topicIndices.rdd.map(lambda x: list(zip(list(map(lambda n: vocabList[n], x.termIndices)), x.termWeights)))
-    topicsWithIndex = topics.zipWithIndex()
-    list(map(lambda x: print_topic(x[0], x[1]), topicsWithIndex.collect()))
-
-
 def clean_text(text):
     text = text.lower()
     items_to_remove = ["'", '"', ';', '!', '.', ',', '-', '_']
--- modelling/recommend.py
+++ modelling/recommend.py
@@ -1,5 +1,29 @@
 from .fit_als import load_als_model
-from .fit_lda import load_lda_model, print_topics
+from .fit_lda import load_lda_model
+
+
+def print_topic(topic, i):
+    print("\n\n")
+    print("Topic: " + str(i))
+    list(map(lambda x: print(str(x[0]) + ": " + str(x[1])), topic)) # x = ( term, weight)
+
+
+def print_topics(ldaPipelineModel):
+    countVectorizer = ldaPipelineModel.stages[3]
+    vocabList = countVectorizer.vocabulary
+    ldaModel = ldaPipelineModel.stages[4]
+    topicIndices = ldaModel.describeTopics(maxTermsPerTopic=5)
+    topics = topicIndices.rdd.map(lambda x: list(zip(list(map(lambda n: vocabList[n], x.termIndices)), x.termWeights)))
+    topicsWithIndex = topics.zipWithIndex()
+    list(map(lambda x: print_topic(x[0], x[1]), topicsWithIndex.collect()))
+
+
+def recommend_based_on_rating(alsModel):
+    pass
+
+
+def recommend_based_on_category(ldaModel, lda_category):
+    pass
 
 
 def main(spark):
