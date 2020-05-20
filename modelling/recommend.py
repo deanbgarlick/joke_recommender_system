41d65abffdbc37f2f2b2890ce36e952a58d72d22
Dean Garlick <deanbgarlick@gmail.com>
2020-05-20 23:27:40 +0100

    Add print topics to recommend.py

--- modelling/recommend.py
+++ modelling/recommend.py
@@ -1,10 +1,11 @@
 from .fit_als import load_als_model
-from .fit_lda import load_lda_model
+from .fit_lda import load_lda_model, print_topics
 
 
 def main(spark):
 
     ldaModel = load_lda_model(spark)
     alsModel = load_als_model()
+    print_topics(ldaModel)
 
 
