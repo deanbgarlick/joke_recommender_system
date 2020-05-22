fef6d6dba61e0f544736ec4fecb08ea49f16917d
deanbgarlick <deanbgarlick@gmail.com>
2020-05-22 21:50:49 +0100

    Change als_data_prep script to replace ratings of 99 with None

--- joke_recommender/als_data_prep.py
+++ joke_recommender/als_data_prep.py
@@ -1,41 +1,38 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.context import SQLContext
-from pyspark.sql.functions import lit, countDistinct
+from pyspark.sql.functions import lit, countDistinct, f
 from pyspark.sql.types import StructField, StructType, IntegerType, DoubleType
 
 
 def main(spark, sqlContext):
 
-    sc = spark.sparkContext
+sc = spark.sparkContext
 
-    inputDF = (
-        spark.read.csv(
-            "s3://aws-emr-resources-257018485161-us-east-1/ratings_3.csv",
-            header="true",
-            inferSchema="true",
-        )
-            .withColumnRenamed("_c0", "userID")
-            .drop("0")
+inputDF = (
+    spark.read.csv(
+        "s3://aws-emr-resources-257018485161-us-east-1/ratings_3.csv",
+        header="true",
+        inferSchema="true",
     )
+        .withColumnRenamed("_c0", "userID")
+        .drop("0")
+)
 
-    fields = [
-        StructField("userID", IntegerType(), True),
-        StructField("jokeID", IntegerType(), True),
-        StructField("rating", DoubleType(), True),
-    ]
-    schema = StructType(fields)
-    allJokeRatingsDF = sqlContext.createDataFrame(sc.emptyRDD(), schema)
+fields = [
+    StructField("userID", IntegerType(), True),
+    StructField("jokeID", IntegerType(), True),
+    StructField("rating", DoubleType(), True),
+]
+schema = StructType(fields)
+allJokeRatingsDF = sqlContext.createDataFrame(sc.emptyRDD(), schema)
 
     for jokeID in inputDF.columns:
         if jokeID != "userID":
            if jokeID != "0":
                 jokeRatings = inputDF.select(["userID", jokeID])
-                jokeRatings = jokeRatings.filter(
-                    (jokeRatings[jokeID] < 10) & (jokeRatings[jokeID] > -10)
-                )
                 jokeRatings = jokeRatings.withColumn("jokeID", lit(int(jokeID)))
                 jokeRatings = jokeRatings.withColumn(
-                    "rating", jokeRatings[jokeID].cast(DoubleType())
+                    "rating", f.when(jokeRatings.jokeID.isin([99.0,99.,99], None)).otherwise(jokeRatings.jokeID.cast(DoubleType()))
                 )
                 jokeRatings = jokeRatings.drop(jokeID)
                 allJokeRatingsDF = allJokeRatingsDF.union(jokeRatings)
